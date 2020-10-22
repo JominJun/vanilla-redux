2f68da49cd299fbaada8967f186861003df1a84e
jominjun <mj3029327@naver.com>
2020-10-22 21:06:11 +0900

    Counter Redux 구현

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { createStore } from "redux";
+
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const PLUS = "PLUS";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case PLUS:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+const handlePlus = () => {
+  countStore.dispatch({ type: PLUS });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+onChange();
+countStore.subscribe(onChange);
+
+plus.addEventListener("click", handlePlus);
+minus.addEventListener("click", handleMinus);
